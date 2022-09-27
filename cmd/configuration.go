100938a4d020522bd57e81e8205e30458396382a
connorsanders <Enterprise22$>
2022-09-27 00:01:44 -0500

    initial commit

--- cmd/configuration.go
+++ cmd/configuration.go
@@ -10,25 +10,28 @@ type Configuration struct {
 	Port           string
 	APIHost        string
 	SessionManager string
-	RedisURL       string
+	RedisHost      string
 	RedisPassword  string
 	ENV            string
 }
 
 // ConfigurationSettings is a function that reads a json configuration file and outputs a Configuration struct
-func ConfigurationSettings(env string) Configuration {
+func ConfigurationSettings(env string) (*Configuration, error) {
 	confFile := "conf.json"
 	if env == "test" {
 		confFile = "test_conf.json"
 	}
-	file, _ := os.Open(confFile)
+	file, err := os.Open(confFile)
+	if err != nil {
+		return nil, err
+	}
 	decoder := json.NewDecoder(file)
 	configurationSettings := Configuration{}
-	err := decoder.Decode(&configurationSettings)
+	err = decoder.Decode(&configurationSettings)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return configurationSettings
+	return &configurationSettings, nil
 }
 
 // InitializeEnvironment sets environmental variables
@@ -36,7 +39,7 @@ func (c *Configuration) InitializeEnvironment() {
 	os.Setenv("PORT", c.Port)
 	os.Setenv("API_HOST", c.APIHost)
 	os.Setenv("SESSION_MANAGER", c.SessionManager)
-	os.Setenv("REDIS_URL", c.RedisURL)
+	os.Setenv("REDIS_HOST", c.RedisHost)
 	os.Setenv("REDIS_PASSWORD", c.RedisPassword)
 	os.Setenv("ENV", c.ENV)
 }
