995a8ee5620d561ecafdc7cd7f6df77d0c54488e
connorsanders <Enterprise22$>
2022-10-27 23:59:41 -0500

    Work on new task overview page

--- models/utils.go
+++ models/utils.go
@@ -19,3 +19,19 @@ func ConvertToDateTime(dateString string) (time.Time, error) {
 	layout := "2006-01-02T15:04"
 	return time.Parse(layout, dateString)
 }
+
+// SplitTasksByStatus inputs a slice of Task and returns a slice of Task for each status type
+func SplitTasksByStatus(tasks []*Task) ([]*Task, []*Task, []*Task) {
+	var ns, ip, com []*Task
+	for _, t := range tasks {
+		switch t.Status {
+		case NOTSTARTED:
+			ns = append(ns, t)
+		case INPROGRESS:
+			ns = append(ip, t)
+		case COMPLETED:
+			ns = append(com, t)
+		}
+	}
+	return ns, ip, com
+}
